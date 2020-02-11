2ee77174ac24ee2f92986974739c745befd7bde6
Nicolas <nicolas.baudvin@gmail.com>
2020-02-11 15:25:02 +0100

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/index.scss';
+
+const App = () => {
+    return (
+        <h1>Modele React avec TypeScript et Redux !</h1>
+    )
+};
+
+export default App;
