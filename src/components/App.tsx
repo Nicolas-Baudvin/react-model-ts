e4e85d3311daba0acf8b4c5995b136b0a0e6aa4e
Nicolas <nicolas.baudvin@gmail.com>
2020-02-12 13:16:51 +0100

    petit exemple d'utilisation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import '../styles/index.scss';
+import "./app.scss";
+import { counterAction } from '../store/counter/actions';
+
+interface counterOptions {
+    nbr: number
+}
+
+interface stateOptions {
+    counter: counterOptions
+}
 
 const App = () => {
-    return (
+    const dispatch = useDispatch();
+    const { nbr } = useSelector((state: stateOptions) => state.counter)
+    console.log(nbr);
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+        e.preventDefault();
+        dispatch(counterAction());
+    }
+
+    return (<div className="App">
         <h1>Modele React avec TypeScript et Redux !</h1>
+        <button className="button" onClick={handleClick}>{nbr}</button>
+    </div>
     )
 };
 
