1a4b047a8c901e74ecbca19e783eb9b0c2f5766c
Nicolas <nicolas.baudvin@gmail.com>
2020-02-12 14:21:55 +0100

    Modification de la configuration pour la minification

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 // Plugins de traitement pour dist/
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
@@ -53,11 +53,16 @@ module.exports = {
         },
         // Minification
         minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: false // passer à true pour JS source maps
-            }),
+            new TerserPlugin({
+                chunkFilter: (chunk) => {
+                  // Exclude uglification for the `vendor` chunk
+                  if (chunk.name === 'vendor') {
+                    return false;
+                  }
+        
+                  return true;
+                },
+              }),
             new OptimizeCSSAssetsPlugin({})
         ]
     },
